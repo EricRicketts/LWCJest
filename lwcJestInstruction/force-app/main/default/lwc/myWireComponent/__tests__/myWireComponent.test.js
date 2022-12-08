e4be256f135996583986bb2863ac7a48b3b6eaa3
Eric Ricketts <eric_ricketts@me.com>
2022-12-07 20:27:19 -0500

    git first wire service test working

--- lwcJestInstruction/force-app/main/default/lwc/myWireComponent/__tests__/myWireComponent.test.js
+++ lwcJestInstruction/force-app/main/default/lwc/myWireComponent/__tests__/myWireComponent.test.js
@@ -1,8 +1,21 @@
+/* eslint-disable @lwc/lwc/no-document-query */
 /* eslint-disable jest/expect-expect */
 import { createElement } from "lwc";
 import MyWireComponent from "c/myWireComponent";
+import getContactList from "@salesforce/apex/ContactController.getContactList";
 
 const mockGetContactList = require("./data/getContactList.json");
+jest.mock(
+  "@salesforce/apex/ContactController.getContactList",
+  () => {
+    const { createApexTestWireAdapter } = require("@salesforce/sfdx-lwc-jest");
+    return {
+      default: createApexTestWireAdapter(jest.fn())
+    };
+  },
+  { virtual: true }
+);
+
 describe("c-my-wire-component", () => {
   let element, expected, result;
   beforeEach(() => {
@@ -13,6 +26,7 @@ describe("c-my-wire-component", () => {
   });
 
   afterEach(() => {
+    jest.clearAllMocks();
     // The jsdom instance is shared across test cases in a single file so reset the DOM
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
@@ -20,6 +34,13 @@ describe("c-my-wire-component", () => {
   });
 
   it("should render the contacts", () => {
-    element = document.querySelector("c-my-wire-component");
+    const testElement = document.querySelector("c-my-wire-component");
+    getContactList.emit(mockGetContactList);
+    return Promise.resolve().then(() => {
+      const pElements = testElement.shadowRoot.querySelectorAll("p");
+      expected = [mockGetContactList.length, mockGetContactList[0].Name];
+      result = [pElements.length, pElements[0].textContent];
+      expect(result).toEqual(expected);
+    });
   });
 });
